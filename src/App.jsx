edab4500d729657b64e865ec08e859c4e11e2b24
Basit612 <sheikhabdulbasit612@gmail.com>
2024-03-13 00:15:59 +0600

    appes code done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
@@ -9,6 +11,11 @@ function App() {
     <>
      
      <Header></Header>
+     
+     <div className='md:flex'>
+     <Blogs></Blogs>
+     <Bookmarks></Bookmarks>
+     </div>
       
     </>
   )
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({blog}) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+Blog.propTypes ={
+  blog: propTypes.object.isRequired
+}
+
+export default Blog;
